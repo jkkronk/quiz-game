0c094232cc55e299eca303c90bc398ea40b94f4b
jonatank <jonatan.kronander@inivation.com>
2024-01-06 17:54:50 +0100

    icons icons icons

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 import time
 import os
@@ -12,48 +14,44 @@ app = Flask(__name__)
 # Route for the home page
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        # Retrieve user's name from the form
-        user_name = request.form['name']
-        if utils.has_played_today(user_name):
-            message = "You have already played today. Please choose a new name."
-            return render_template('home.html', message=message)
-
-        return redirect(url_for('video', user_name=user_name))
     return render_template('home.html')
 
+
 # Route for the video page
-@app.route('/video/<user_name>')
-def video(user_name):
+@app.route('/video')
+def video():
     # Path to the text file with the answer
     correct_answer = utils.get_answer(os.path.join(app.static_folder, 'quiz.json'))
-
     start_time = time.time()
-    return render_template('video.html', user_name=user_name, start_time=start_time, correct_answer=correct_answer)
+    return render_template('video.html', start_time=start_time, correct_answer=correct_answer)
 
-
-@app.route('/submit_answer/<user_name>', methods=['POST'])
-def submit_answer(user_name):
-    video_file_path = os.path.join(app.static_folder, 'quiz.mp4')
-    start_time = float(request.form['start_time'])
-    end_time = time.time()
-    time_taken = end_time - start_time
-    score = utils.calculate_score(time_taken, video_file_path)
-    utils.save_high_score(user_name, score)
-
-    return redirect(url_for('score', user_name=user_name, score=score))
-
-# Route for the score page
-@app.route('/score/<user_name>/<score>')
-def score(user_name, score):
-    daily_high_scores = utils.get_daily_high_scores()  # This function should return today's high scores
-    return render_template('score.html', user_name=user_name, score=score, daily_high_scores=daily_high_scores)
 
 @app.route('/high_scores')
 def high_scores():
     all_time_high_scores = utils.get_all_time_high_scores()  # Function to get all-time high scores
     daily_high_scores = utils.get_daily_high_scores()  # Function to get today's high scores
     return render_template('high_scores.html', all_time_high_scores=all_time_high_scores, daily_high_scores=daily_high_scores)
+
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+
+@app.route('/submit_answer', methods=['POST'])
+def submit_answer():
+    video_file_path = os.path.join(app.static_folder, 'quiz.mp4')
+    start_time = float(request.form['start_time'])
+    end_time = time.time()
+    time_taken = end_time - start_time
+    score = utils.calculate_score(time_taken, video_file_path)
+    return redirect(url_for('score', score=score))
+
+# Route for the score page
+@app.route('/score/<score>')
+def score(score):
+    daily_high_scores = utils.get_daily_high_scores()  # This function should return today's high scores
+    return render_template('score.html', score=score, daily_high_scores=daily_high_scores)
 
 
 @app.route('/explanations')
