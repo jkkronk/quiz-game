e1517959566683bc2b428b2eaee1a987a718ec6e
jonatank <jonatan.kronander@inivation.com>
2024-01-07 19:42:49 +0100

    update quiz 10 min

--- update_quiz.py
+++ update_quiz.py
@@ -1,5 +1,13 @@
 import utils
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
 
 if __name__ == '__main__':
-    utils.clear_daily_high_scores()
-    utils.create_new_video()
+    # Initialize Scheduler
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=utils.create_new_video, trigger="interval", minutes=10)
+    scheduler.add_job(func=utils.clear_daily_high_scores, trigger="interval", minutes=10)
+    scheduler.start()
+
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: scheduler.shutdown())
--- update_quiz.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python3 update_quiz.py
-git add .
-git commit -m "update quiz"
-git push origin newhome
