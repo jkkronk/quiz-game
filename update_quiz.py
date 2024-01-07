c7294796464e1fe9ce7b2e993589474242c667f2
jonatank <jonatan.kronander@inivation.com>
2024-01-07 20:12:20 +0100

    update daily

--- update_quiz.py
+++ update_quiz.py
@@ -3,11 +3,14 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
 
 if __name__ == '__main__':
-    # Initialize Scheduler
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(func=utils.create_new_video, trigger="interval", minutes=10)
-    scheduler.add_job(func=utils.clear_daily_high_scores, trigger="interval", minutes=10)
-    scheduler.start()
+    utils.create_new_video()
+    utils.clear_daily_high_scores()
 
-    # Shut down the scheduler when exiting the app
-    atexit.register(lambda: scheduler.shutdown())
+    # # Initialize Scheduler
+    # scheduler = BackgroundScheduler()
+    # scheduler.add_job(func=utils.create_new_video, trigger="interval", minutes=10)
+    # scheduler.add_job(func=utils.clear_daily_high_scores, trigger="interval", minutes=10)
+    # scheduler.start()
+    #
+    # # Shut down the scheduler when exiting the app
+    # atexit.register(lambda: scheduler.shutdown())
